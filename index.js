83b3bc5300aa2611fb88f9b51f2433958060f085
Rick Martens <mogarian1999@gmail.com>
2022-12-09 17:24:56 +0100

    Fully functional
    Need virtual audio cable to combine both inputs to single output.

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const fs = require('fs')
 const path = require('path')
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+let mediaDevices;
 
 function fillTable() {
     let table = document.getElementById('soundBody');
-    
+
     const directoryPath = path.join(__dirname, 'sounds')
 
-    fs.readdir(directoryPath, function(err, files) {
+    fs.readdir(directoryPath, function (err, files) {
         if (err) {
-          return console.log('Unable to scan directory: ' + err);
+            return console.log('Unable to scan directory: ' + err);
         }
-      
-        files.forEach(function(file) {
+
+        files.forEach(function (file) {
             const tr = document.createElement('tr');
             const tdName = document.createElement('td');
             const tdOpen = document.createElement('td');
             const tdPause = document.createElement('td')
             const btnOpen = document.createElement('button');
-            const btnPause = document.createElement('button')
-        
+            const btnPause = document.createElement('button');
+            const audioElement = document.createElement('audio')
+
             tdName.innerText = file;
+            audioElement.src = path.join(directoryPath, file)
             btnOpen.innerText = 'Play';
             btnPause.innerText = 'Pause';
             let beat = new Audio(path.join(directoryPath, file))
-            btnOpen.addEventListener('click', function() {    
+            btnOpen.addEventListener('click', function () {
+                var dropdown = document.getElementById("devices");
+                var deviceId = dropdown.options[dropdown.selectedIndex].value;
+                if (deviceId) {
+                    beat.setSinkId(deviceId);
+                }
                 beat.play();
             });
-            btnPause.addEventListener('click', function() {
+            btnPause.addEventListener('click', function () {
                 beat.load();
             })
-        
+
             tdOpen.appendChild(btnOpen);
             tdPause.appendChild(btnPause)
             tr.appendChild(tdName);
@@ -40,4 +50,34 @@ function fillTable() {
     });
 }
 
+function getAudioOutputDevices() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+        console.log("Your browser does not support this feature.");
+        return;
+    }
+
+    navigator.mediaDevices.enumerateDevices()
+        .then(function (devices) {
+            devices = devices.filter(function (device) {
+                return device.kind === "audiooutput";
+            });
+
+            if (devices.length === 0) {
+                console.log("No audio output devices were found.");
+            } else {
+                var dropdown = document.getElementById("devices");
+                for (var i = 0; i < devices.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = devices[i].deviceId;
+                    option.text = devices[i].label;
+                    dropdown.add(option);
+                }
+            }
+        })
+        .catch(function (err) {
+            console.log("An error occurred: ", err);
+        });
+}
+
+getAudioOutputDevices();
 fillTable();
